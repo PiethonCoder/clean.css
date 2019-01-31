45e22597b742dfcf10bb12c474fae233be0d2c09
PiethonCoder <austinthemichaud@gmail.com>
2019-01-31 15:31:59 -0500

    Add files via upload

---
+++ site/main.js
@@ -0,0 +1,11 @@
+$("#button").click(function() {
+    $("#button-image").attr("src", "https://images.pexels.com/photos/1188214/pexels-photo-1188214.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+})
+
+$("#button-a").click(function() {
+    $("#button-image").attr("src", "https://images.pexels.com/photos/1477166/pexels-photo-1477166.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+})
+
+$("#button-b").click(function() {
+    $("#button-image").attr("src", "https://images.pexels.com/photos/250716/pexels-photo-250716.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+})
